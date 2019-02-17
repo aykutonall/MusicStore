59aeed19ce6a96b7d6bad638cef33ec7b25d5a78
aykutonall <aykutonall@gmail.com>
2019-02-17 19:13:41 +0300

    last

---
+++ MusicStore/MusicStore/Controllers/StoreManagerController.cs
@@ -0,0 +1,87 @@
+﻿using MusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class StoreManagerController : Controller
+    {
+        private MusicStoreEntities db = new MusicStoreEntities();
+
+        // GET: StoreManager
+        public ActionResult Index()
+        {
+            var albums = db.Albums.Include(b => b.Genre).Include(b => b.Artist);
+            return View(albums.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name");
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Album album)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Albums.Add(album);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Album album = db.Albums.Find(id);
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Album album)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Entry(album).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Album album = db.Albums.Find(id);
+            return View(album);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Album album = db.Albums.Find(id);
+            db.Albums.Remove(album);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Album album = db.Albums.Find(id);
+            return View(album);
+        }
+    }
+}
--- MusicStore/MusicStore/Models/MusicStoreEntities.cs
+++ MusicStore/MusicStore/Models/MusicStoreEntities.cs
@@ -9,7 +9,12 @@ namespace MusicStore.Models
 {
     public class MusicStoreEntities : DbContext
     {
+        public MusicStoreEntities() : base()
+        {
+
+        }
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
     }
 }
